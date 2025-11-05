818c17e611652c81f878bd71b0ec8ba70cfe0cc6
ines ponce <inesponcetandil@gmail.com>
2025-11-05 16:02:53 -0300

    creacion de pagina de juegos

---
+++ JUEGO DEL AHORCADO/script.js
@@ -0,0 +1,40 @@
+const replaceAt = (string, character, index) => {
+    return string.substring(0, index) + character + string.substring(index + character.length);
+}
+
+const words = ['gato', 'rana', 'perro', 'pato'];
+const secretWord = words[Math.floor(Math.random() * words.length)];
+let hiddenWord = secretWord.replace(/./g, "_ ");
+document.querySelector('.hiddenWord').innerHTML = hiddenWord;
+let errorCounter = 0;
+alert(secretWord);
+const evaluateWord = () => {
+    const letter = document.querySelector('input').value;
+    document.querySelector('input').value = '';
+    let error = true
+     for (let i = 0; i < secretWord.length; i++){
+         if (secretWord [i] === letter) {
+            hiddenWord = replaceAt(hiddenWord, letter, i * 2);
+            error = false;
+         }
+     }
+     document.querySelector('.hiddenWord').innerHTML = hiddenWord;
+     
+     if (error) {
+        errorCounter++;
+        document.querySelector('.ahorcado').style.backgroundPosition = -errorCounter * 300 + 'px';
+        if (errorCounter === 7) {
+            document.querySelector('.info').innerHTML = "<h1>Has perdido!!!❌👎😞😭😭😭</h1>";
+        }
+     }
+
+
+
+     if  (!hiddenWord.includes("_")) {
+     document.querySelector('.info').innerHTML = "<h1>Has ganado!!!!✅👍😊🎉🎉🎉</h1>";
+    }
+   
+    }
+
+document.querySelector('button').addEventListener('click', evaluateWord);
+   
